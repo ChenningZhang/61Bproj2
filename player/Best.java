f1107559ab01fe124349b3015f7f0a14a8f0b3e5
ChenningZhang <czhang1306@berkeley.edu>
2014-11-13 20:31:41 -0800

    added files

---
+++ player/Best.java
@@ -0,0 +1,14 @@
+/* Best.java */
+ 
+package player;
+ 
+public class Best {
+ 
+    protected Move move;
+    protected int score;
+ 
+    public Best() {
+        move = new Move();
+        score = 0;
+    }
+}
